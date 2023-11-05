b21707f09a0b2b6e0369abb2105bb0023573317b
zerokii <139667384+zerokii@users.noreply.github.com>
2023-11-05 11:10:49 +0800

    manipulate text style

--- Project1_M1_M5/src/screens/SecondScreen.js
+++ Project1_M1_M5/src/screens/SecondScreen.js
@@ -7,14 +7,23 @@ const SecondScreen = () => {
         <View style={{ flex: 1, backgroundColor: 'lavender', justifyContent: "center", alignItems: "center" }}>
 
             <View style={{ backgroundColor: 'skyblue', marginLeft: 17, marginRight: 17, marginBottom: 5, padding: 7, borderWidth: 1, borderColor: "red", borderStyle: "dashed", borderRadius: 7 }}>
-                <Text>
+                <Text style={{fontWeight: "bold", textAlign: "center"}}>
                     Mobile Legends
+                </Text>
+                <Text style={{textAlign: "justify"}}>
+                    <Text style={{ color: "purple" }}>Mobile Legends</Text> is a multiplayer online battle arena (MOBA) game. The two
+                    opposing teams fight to reach and destroy the enemy's base while defending
+                    their own base for control of a path.
                 </Text>
             </View>
 
             <View style={{ backgroundColor: 'cornflowerblue', marginLeft: 17, marginRight: 17, marginTop: 5, padding: 7, borderWidth: 1, borderColor: "red", borderStyle: "dotted", borderRadius: 7 }}>
-                <Text>
+                <Text style={{fontWeight: "bold", textAlign: "center"}}>
                     PUBG
+                </Text>
+                <Text style={{textAlign: "justify"}}>
+                    <Text style={{ color: "purple" }}>PlayerUnknown's Battlegrounds</Text>, better known as PUBG, is a multiplayer battle royale
+                    game in which players drop onto an island and fight to be the last one left standing.
                 </Text>
             </View>
 
