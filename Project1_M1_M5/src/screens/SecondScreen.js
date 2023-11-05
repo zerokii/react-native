739511dc8140e92507e4a9e6a50aeb3a6003e1a3
zerokii <139667384+zerokii@users.noreply.github.com>
2023-11-05 11:02:01 +0800

    edit margin border and padding

--- Project1_M1_M5/src/screens/SecondScreen.js
+++ Project1_M1_M5/src/screens/SecondScreen.js
@@ -4,15 +4,15 @@ import { ceil } from "react-native-reanimated";
 
 const SecondScreen = () => {
     return (
-        <View style={{ flex: 1, backgroundColor: 'lavender', justifyContent:"center", alignItems:"center"}}>
+        <View style={{ flex: 1, backgroundColor: 'lavender', justifyContent: "center", alignItems: "center" }}>
 
-            <View style={{ backgroundColor: 'skyblue' }}>
+            <View style={{ backgroundColor: 'skyblue', marginLeft: 17, marginRight: 17, marginBottom: 5, padding: 7, borderWidth: 1, borderColor: "red", borderStyle: "dashed", borderRadius: 7 }}>
                 <Text>
                     Mobile Legends
                 </Text>
             </View>
 
-            <View style={{ backgroundColor: 'cornflowerblue' }}>
+            <View style={{ backgroundColor: 'cornflowerblue', marginLeft: 17, marginRight: 17, marginTop: 5, padding: 7, borderWidth: 1, borderColor: "red", borderStyle: "dotted", borderRadius: 7 }}>
                 <Text>
                     PUBG
                 </Text>
