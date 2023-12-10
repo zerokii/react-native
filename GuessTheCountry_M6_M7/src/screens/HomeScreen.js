bbff5f1f405f050d6a26c1209152d2d7efabd1bd
zerokii <139667384+zerokii@users.noreply.github.com>
2023-12-10 11:56:09 +0800

    create guess the country screen

---
+++ GuessTheCountry_M6_M7/src/screens/HomeScreen.js
@@ -0,0 +1,60 @@
+
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+const HomeScreen = () => {
+    const [number, setNumber] = useState(0);
+
+    const decreaseNumber = () => {
+        setNumber(number - 1);
+    };
+
+    const increaseNumber = () => {
+        setNumber(number + 1);
+    };
+
+    useEffect(() => {
+        setNumber(10);
+        alert('You can increase or decrease the number');
+    }, []);
+
+    return (
+        <View style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'row'
+        }}>
+            <View style={{
+                margin: 8,
+                padding: 8,
+                borderWidth: 1
+            }}>
+                <TouchableOpacity
+                    onPress={() => decreaseNumber()}
+                >
+                    <Text style={{ fontSize: 50 }}> - </Text>
+                </TouchableOpacity>
+            </View>
+            <View style={{
+                margin: 8,
+                padding: 8
+            }}>
+                <Text style={{ fontSize: 50 }}>{number}</Text>
+            </View>
+            <View style={{
+                margin: 8,
+                padding: 8,
+                borderWidth: 1
+            }}>
+                <TouchableOpacity
+                    onPress={() => increaseNumber()}
+                >
+                    <Text style={{ fontSize: 50 }}> + </Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+};
+
+export default HomeScreen;
