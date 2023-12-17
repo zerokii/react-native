395cc79c0c5baa08640ef8bbe5fd615ec50e925d
zerokii <139667384+zerokii@users.noreply.github.com>
2023-12-17 10:36:49 +0800

    add win screen

--- GuessTheCountry_M6_M7/App.tsx
+++ GuessTheCountry_M6_M7/App.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import HomeScreen from "./src/screens/HomeScreen";
 import GuessTheCountryScreen from "./src/screens/GuessTheCountryScreen";
+import WinScreen from "./src/screens/WinScreen";
+import MainNavigator from "./src/navigator/MainNavigator";
+import 'react-native-gesture-handler';
 
 const App: React.FC = ()=>{
   return(
     <SafeAreaProvider>
-      <GuessTheCountryScreen/>
+      <MainNavigator/>
     </SafeAreaProvider>
   )
 }
