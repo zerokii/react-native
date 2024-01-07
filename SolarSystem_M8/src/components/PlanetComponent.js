c636d8897ce180cee5d11370cd679567c23e8ecd
zerokii <139667384+zerokii@users.noreply.github.com>
2024-01-07 12:46:15 +0800

    add onpress and touchable opacity

--- SolarSystem_M8/src/components/PlanetComponent.js
+++ SolarSystem_M8/src/components/PlanetComponent.js
@@ -1,40 +1,47 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 
 export const Planet = (props) => {
 
-    const { planetImage, planetName, planetDescription  } = props;
+    const { planetImage, planetName, planetDescription } = props;
+
+    const clickedPlanet = (planet) => {
+        capitilizedPlanet = planet[0].toUpperCase() + planet.slice(1);
+        alert(`You clicked ${capitilizedPlanet}!`);
+    };
 
     return (
-        <View style={{
-            flexDirection: "row",
-            backgroundColor: "wheat",
-            margin: 16,
-            padding: 16,
-            borderRadius: 20,
-            borderColor: "green",
-            borderWidth: 1
-        }}>
+        <TouchableOpacity onPress={() => clickedPlanet(planetName)}>
+            <View style={{
+                flexDirection: "row",
+                backgroundColor: "wheat",
+                margin: 16,
+                padding: 16,
+                borderRadius: 20,
+                borderColor: "green",
+                borderWidth: 1
+            }}>
 
-            <Image
-                style={{
-                    width: 100,
-                    height: 100,
-                }}
-                source={planetImage}
-            />
+                <Image
+                    style={{
+                        width: 100,
+                        height: 100,
+                    }}
+                    source={planetImage}
+                />
 
-            <View style={{ flex: 1, marginLeft: 8, marginRight: 8, justifyContent: "center" }}>
-                <Text style={{ fontSize: 20, fontWeight: "bold", textTransform: "capitalize" }}>
-                    {planetName}
-                </Text>
+                <View style={{ flex: 1, marginLeft: 8, marginRight: 8, justifyContent: "center" }}>
+                    <Text style={{ fontSize: 20, fontWeight: "bold", textTransform: "capitalize" }}>
+                        {planetName}
+                    </Text>
 
-                <Text>
-                    {planetDescription}
-                </Text>
-            </View>       
+                    <Text>
+                        {planetDescription}
+                    </Text>
+                </View>
 
-        </View>
+            </View>
+        </TouchableOpacity>
     )
 
 };
--- SolarSystem_M8/src/screens/SolarSystemScreen.js
+++ SolarSystem_M8/src/screens/SolarSystemScreen.js
@@ -17,53 +17,55 @@ const SolarSystemScreen = () => {
                 </Text>
             </View>
 
-            <Planet
-                planetImage={require("../../assets/images/mercury.png")}
-                planetName="mercury"
-                planetDescription="Mercury is the fastest planet, zipping around the sun every 88 earth days"
-            />
+            <ScrollView>
+                <Planet
+                    planetImage={require("../../assets/images/mercury.png")}
+                    planetName="mercury"
+                    planetDescription="Mercury is the fastest planet, zipping around the sun every 88 earth days"
+                />
 
-            <Planet
-                planetImage={require("../../assets/images/venus.png")}
-                planetName="venus"
-                planetDescription="Venus spins slowly in the opposite direction from most planets"
-            />
+                <Planet
+                    planetImage={require("../../assets/images/venus.png")}
+                    planetName="venus"
+                    planetDescription="Venus spins slowly in the opposite direction from most planets"
+                />
 
-            <Planet
-                planetImage={require("../../assets/images/earth.png")}
-                planetName="earth"
-                planetDescription="Earth is the only place we know of so far that's inhabited by living things"
-            />
+                <Planet
+                    planetImage={require("../../assets/images/earth.png")}
+                    planetName="earth"
+                    planetDescription="Earth is the only place we know of so far that's inhabited by living things"
+                />
 
-            <Planet
-                planetImage={require("../../assets/images/mars.png")}
-                planetName="mars"
-                planetDescription="Mars is a dusty, cold, desert world with a very thin atmosphere"
-            />
+                <Planet
+                    planetImage={require("../../assets/images/mars.png")}
+                    planetName="mars"
+                    planetDescription="Mars is a dusty, cold, desert world with a very thin atmosphere"
+                />
 
-            <Planet
-                planetImage={require("../../assets/images/jupiter.png")}
-                planetName="jupiter"
-                planetDescription="Jupiter is more than twice as massive than the other planets of our solar system combined"
-            />
+                <Planet
+                    planetImage={require("../../assets/images/jupiter.png")}
+                    planetName="jupiter"
+                    planetDescription="Jupiter is more than twice as massive than the other planets of our solar system combined"
+                />
 
-            <Planet
-                planetImage={require("../../assets/images/saturn.png")}
-                planetName="saturn"
-                planetDescription="Adorned with a dazzling, complex system of icy rings, Saturn is unique in our solar system"
-            />
+                <Planet
+                    planetImage={require("../../assets/images/saturn.png")}
+                    planetName="saturn"
+                    planetDescription="Adorned with a dazzling, complex system of icy rings, Saturn is unique in our solar system"
+                />
 
-            <Planet
-                planetImage={require("../../assets/images/uranus.png")}
-                planetName="uranus"
-                planetDescription="Uranus rotates at a nearly 90-degree angle from the plane of its orbit"
-            />
+                <Planet
+                    planetImage={require("../../assets/images/uranus.png")}
+                    planetName="uranus"
+                    planetDescription="Uranus rotates at a nearly 90-degree angle from the plane of its orbit"
+                />
 
-            <Planet
-                planetImage={require("../../assets/images/neptune.png")}
-                planetName="neptune"
-                planetDescription="Neptune is dark, cold, and whipped by supersonic winds"
-            />
+                <Planet
+                    planetImage={require("../../assets/images/neptune.png")}
+                    planetName="neptune"
+                    planetDescription="Neptune is dark, cold, and whipped by supersonic winds"
+                />
+            </ScrollView>
 
         </View>
     )
