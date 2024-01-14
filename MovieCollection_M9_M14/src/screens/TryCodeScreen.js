268368d3b7f73fff25453b44e4f6108ed9b57f34
zerokii <139667384+zerokii@users.noreply.github.com>
2024-01-14 10:56:50 +0800

    Update TryCodeScreen.js

--- MovieCollection_M9_M14/src/screens/TryCodeScreen.js
+++ MovieCollection_M9_M14/src/screens/TryCodeScreen.js
@@ -6,7 +6,12 @@ const TryCodeScreen = () => {
 
     return (
         <View style={{ flex: 1 }}>
-
+            <Text>
+                An array of objects lets you store multiple values in a single variable. It
+                stores a fixed-size sequential collection of elements of the same type. An
+                array is used to store a collection of data, but it is often more useful to
+                think of an array as a collection of variables of the same type.
+            </Text>
             <FlatList
                 contentContainerStyle={{ padding: 8 }}
                 data={userData}
@@ -18,12 +23,19 @@ const TryCodeScreen = () => {
                             backgroundColor: "lavender",
                             borderWidth: 1
                         }}>
+                            <Image
+                                source={{ uri: item.imageLink }}
+                                style={{
+                                    width: 100,
+                                    height: 100
+                                }}
+                            />
                             <Text>{item.name}</Text>
                         </View>
                     )
                 }}
             />
-            
+
         </View>
     )
 };
