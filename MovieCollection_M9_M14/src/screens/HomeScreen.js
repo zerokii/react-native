4407b3ffe7f6f5e7f1c398964a9b9ae2f0c1d48f
zerokii <139667384+zerokii@users.noreply.github.com>
2024-01-14 11:07:56 +0800

    create stylesheet

--- MovieCollection_M9_M14/src/screens/HomeScreen.js
+++ MovieCollection_M9_M14/src/screens/HomeScreen.js
@@ -1,19 +1,45 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
 import { movieData } from '../../data/MovieData';
 
 const HomeScreen = () => {
-
     return (
         <View style={styles.mainContainer}>
-            <Text>Home Screen</Text>
+            <FlatList
+                data={movieData}
+                keyExtractor={(item) => item.id}
+                contentContainerStyle={styles.flatListContainer}
+                renderItem={({ item }) => {
+                    return (
+                        <View style={styles.dataContainer}>
+                            <Image
+                                style={styles.movieImage}
+                                source={{ uri: item.imageLink }}
+
+                            />
+                        </View>
+                    )
+                }}
+            />
         </View>
     )
 };
+
 const styles = StyleSheet.create({
     mainContainer: {
         flex: 1
     },
+    flatListContainer: {
+        padding: 8
+    },
+    movieImage: {
+        width: 130,
+        height: 200,
+        borderRadius: 10
+    },
+    dataContainer: {
+        margin: 8
+    }
 });
 
 export default HomeScreen;
