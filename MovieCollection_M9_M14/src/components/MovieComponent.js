d284a45e50e4b73ec32133e16b6f46b5ca178299
zerokii <139667384+zerokii@users.noreply.github.com>
2024-02-04 11:24:56 +0800

    add movie details

--- MovieCollection_M9_M14/src/components/MovieComponent.js
+++ MovieCollection_M9_M14/src/components/MovieComponent.js
@@ -38,6 +38,53 @@ export const ShowMovie = (props) => {
     )
 };
 
+export const MovieExplanation = (props) => {
+    const { name, value, isRating, rating } = props;
+    return (
+        <View style={styles.mainContainer}>
+            <View style={styles.nameContainer}>
+                <Text style={styles.generalFontSize}>{name}</Text>
+            </View>
+            <Text style={styles.generalFontSize}>: </Text>
+            <View style={styles.valueContainer}>
+                {
+                    isRating ?
+                        rating === 5 ?
+                            <Image
+                                style={styles.ratingImage}
+                                source={require("../../assets/images/five-stars.png")}
+                            />
+                            :
+                            rating === 4 ?
+                                <Image
+                                    style={styles.ratingImage}
+                                    source={require("../../assets/images/four-stars.png")}
+                                />
+                                :
+                                rating === 3 ?
+                                    <Image
+                                        style={styles.ratingImage}
+                                        source={require("../../assets/images/three-stars.png")}
+                                    />
+                                    :
+                                    rating === 2 ?
+                                        <Image
+                                            style={styles.ratingImage}
+                                            source={require("../../assets/images/two-stars.png")}
+                                        />
+                                        :
+                                        <Image
+                                            style={styles.ratingImage}
+                                            source={require("../../assets/images/star.png")}
+                                        />
+                        :
+                        <Text style={styles.textValue}>{value}</Text>
+                }
+            </View>
+        </View>
+    )
+};
+
 const styles = StyleSheet.create({
     horizontalDataContainer: {
         margin: 8,
@@ -68,5 +115,26 @@ const styles = StyleSheet.create({
     },
     viewersText: {
         marginLeft: 8
-    }
+    },
+    mainContainer: {
+        flexDirection: "row",
+        margin: 8
+    },
+    nameContainer: {
+        flex: 1
+    },
+    generalFontSize: {
+        fontSize: 16
+    },
+    valueContainer: {
+        flex: 2
+    },
+    textValue: {
+        textAlign: "justify",
+        fontSize: 16
+    },
+    ratingImage: {
+        width: 100,
+        height: 20
+    },
 });
