da2006eac575b1c07dbdfce67be4fb87ba50653e
zerokii <139667384+zerokii@users.noreply.github.com>
2024-02-04 10:54:08 +0800

    tidy up moviecomponent.js

--- MovieCollection_M9_M14/src/components/MovieComponent.js
+++ MovieCollection_M9_M14/src/components/MovieComponent.js
@@ -2,39 +2,6 @@ import React from "react";
 import { View, Text, Image, StyleSheet } from "react-native";
 import { Icon } from "react-native-elements";
 
-const styles = StyleSheet.create({
-    horizontalDataContainer: {
-        margin: 8,
-        width: 180,
-        alignItems: "center",
-        borderColor: "#96ceb4",
-        borderWidth: 2,
-        borderRadius: 10,
-        padding: 16
-    },
-    horizontalTitleContainer: {
-        marginTop: 8,
-        marginBottom: 8
-    },
-    horizontalTitle: {
-        fontSize: 18,
-        fontWeight: "bold",
-        textAlign: "center"
-    },
-    movieImage: {
-        width: 130,
-        height: 200,
-        borderRadius: 10
-    },
-    viewersContainer: {
-        flexDirection: "row",
-        alignItems: "center"
-    },
-    viewersText: {
-        marginLeft: 8
-    }
-});
-
 const numberWithCommas = (number) => {
     return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
@@ -70,3 +37,36 @@ export const ShowMovie = (props) => {
         </View>
     )
 };
+
+const styles = StyleSheet.create({
+    horizontalDataContainer: {
+        margin: 8,
+        width: 180,
+        alignItems: "center",
+        borderColor: "#96ceb4",
+        borderWidth: 2,
+        borderRadius: 10,
+        padding: 16
+    },
+    horizontalTitleContainer: {
+        marginTop: 8,
+        marginBottom: 8
+    },
+    horizontalTitle: {
+        fontSize: 18,
+        fontWeight: "bold",
+        textAlign: "center"
+    },
+    movieImage: {
+        width: 130,
+        height: 200,
+        borderRadius: 10
+    },
+    viewersContainer: {
+        flexDirection: "row",
+        alignItems: "center"
+    },
+    viewersText: {
+        marginLeft: 8
+    }
+});
